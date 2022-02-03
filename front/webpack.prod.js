c0d00505c2504f814a92989d3cbb63285389b3ec
EunYoung <pandamon24@naver.com>
2022-02-04 02:22:02 +0900

    build 설정 변경

--- front/webpack.prod.js
+++ front/webpack.prod.js
@@ -1,31 +1,50 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval",
+  mode: "production",
+  devtool: "source-map",
   entry: {
     app: "./src/index",
   },
   resolve: {
-    extensions: [".ts", ".tsx", "js"],
+    extensions: [".ts", ".tsx", ".js"],
   },
   module: {
     rules: [
       {
-        test: /\.jsx?/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-        },
+        test: /\.tsx?/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+          },
+        ],
         exclude: /node_modules/,
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: { minimize: true },
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
   output: {
     path: path.resolve(__dirname + "/dist"),
     filename: "bundle.js",
     publicPath: "/",
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
 };
