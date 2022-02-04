1ebc746e0862e0689016f042f4c62e5132702efc
EunYoung <pandamon24@naver.com>
2022-02-04 23:42:12 +0900

    listItem click이벤트 위치변경

--- front/src/components/ListItem.tsx
+++ front/src/components/ListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import { updateTodoThunk } from "../actions/TodoActions";
 import { initState } from "./Todolist";
 import { useDispatch } from "react-redux";
@@ -15,28 +15,28 @@ type ListItemProps = {
 
 const ListItem = ({ isFinished, todo, id }: ListItemProps) => {
   const DeleteRef = useRef<boolean>(false);
+  const [check, setCheck] = useState<boolean>(isFinished);
   const dispatch = useDispatch();
 
   const handleMouseEnter = () => {
     DeleteRef.current = true;
+    setCheck((prev) => !prev);
   };
 
   const handleMouseLeave = () => {
     DeleteRef.current = false;
+    setCheck((prev) => !prev);
   };
 
   const handleClick = () => {
     updateTodoThunk(dispatch, () => initState, { id, isFinished });
+    setCheck((prev) => !prev);
   };
 
   return (
-    <StyledLi
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      onClick={handleClick}
-    >
-      <button type="button">
-        {isFinished ? <BiCheckboxChecked /> : <BiCheckbox />}
+    <StyledLi onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+      <button type="button" onClick={handleClick}>
+        {check ? <BiCheckboxChecked /> : <BiCheckbox />}
       </button>
       {todo}
       {DeleteRef.current ? <DeleteButton id={id} /> : null}
