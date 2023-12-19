e8dc31f2b10d43b619d19c6c4fa2b21c2eb8af57
eunung <pandamon24@naver.com>
2023-12-19 18:44:46 +0900

    feat: webpack-bundle-analyzer dev 모드에만 on

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = function (env) {
         filename: "index.html",
         inject: "body",
       }),
-      new BundleAnalyzerPlugin(),
+      isDevMode && new BundleAnalyzerPlugin(),
     ].filter(Boolean),
     output: {
       path: path.resolve(__dirname + "/dist"),
