014cfd064e30fcbd12414c22ce8858c5888e75ca
eunung <pandamon24@naver.com>
2023-12-20 03:04:27 +0900

    feat: github actions vercel 변경

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -3,16 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = function (env) {
-  const currentPath = path.join(__dirname);
-  const basePath = currentPath + "/.env";
-  const envPath = basePath + "." + env.mode;
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
-  const isDevMode = envKeys["NODE_ENV"] === "development";
+  const isDevMode = env.mode === "development";
 
   return {
     mode: isDevMode ? "development" : "production",
