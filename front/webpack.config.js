7a14036bf276c285566b9dba5a79df4ab9098b12
eunung <pandamon24@naver.com>
2023-12-19 18:39:15 +0900

    feat: webpack 파일 통합(dev, prod)

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,66 +1,61 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = function (env) {
-  const isDevMode = env.development;
+  const isDevMode = env.mode === "development";
 
-  return (
-    {
-      mode: isDevMode ? "development" : "production",
-      devtool: isDevMode ? "inline-source-map" : "source-map",
-      entry: {
-        app: "./src/index",
-      },
-      resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-      },
-      module: {
-        rules: [
-          {
-            test: /\.tsx?/,
-            use: [
-              {
-                loader: "babel-loader",
-                // options: {
-                //   plugins: !isDevMode ? ["transfrom-remove-console", { exclude: ["error", "warn"] }] : []
-                // }
-              },
-              {
-                loader: "ts-loader",
-              },
-            ],
-            exclude: /node_modules/,
-          },
-          {
-            test: /\.html$/,
-            loader: "html-loader",
-            options: { minimize: true },
-          },
-        ],
-      },
-      plugins: [
-        new HtmlWebpackPlugin({
-          template: "./index.html",
-          filename: "index.html",
-        }),
-        !isDevMode && new CleanWebpackPlugin(),
-      ].filter(Boolean),
-      output: {
-        path: path.resolve(__dirname + "/dist"),
-        filename: "bundle.[chunkhash].js",
-        publicPath: "/",
-      },
-      optimization: {
-        minimize: true,
-        runtimeChunk: 'single',
-      },
-      ...(isDevMode && {
-        devServer: {
-          static: { directory: path.join(__dirname, 'dist') },
-          hot: true,
-        }
+  return {
+    mode: isDevMode ? "development" : "production",
+    devtool: isDevMode ? "inline-source-map" : "source-map",
+    entry: {
+      app: "./src/index",
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts|js)$/,
+          use: [
+            {
+              loader: "ts-loader",
+            },
+          ],
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.html$/,
+          loader: "html-loader",
+          options: { minimize: true },
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        filename: "index.html",
+        inject: "body",
       }),
-    }
-)
-}
+      new BundleAnalyzerPlugin(),
+    ].filter(Boolean),
+    output: {
+      path: path.resolve(__dirname + "/dist"),
+      filename: "bundle.[chunkhash].js",
+      publicPath: "/",
+      clean: true,
+    },
+    optimization: {
+      minimize: true,
+      runtimeChunk: "single",
+    },
+    ...(isDevMode && {
+      devServer: {
+        static: { directory: path.join(__dirname, "dist") },
+        hot: true,
+      },
+    }),
+  };
+};
