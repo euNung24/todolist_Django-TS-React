4116161161123dc80fb566df6234205934ef9217
eunung <pandamon24@naver.com>
2023-12-20 01:57:27 +0900

    feat: webpack.config 정리

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -1,26 +1,8 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
-const webpack = require("webpack");
-const dotenv = require("dotenv");
-const fs = require("fs");
 
 module.exports = function (env) {
   const isDevMode = env.mode === "development";
-
-  const currentPath = path.join(__dirname);
-  const basePath = currentPath + "/.env";
-  const envPath = basePath + "." + env.mode;
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-  // call dotenv and it will return an Object with a parsed key
-  const fileEnv = dotenv.config({ path: finalPath }).parsed;
-
-  // reduce it to a nice object, the same as before
-  const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
 
   return {
     mode: isDevMode ? "development" : "production",
@@ -55,7 +37,6 @@ module.exports = function (env) {
         filename: "index.html",
         inject: "body",
       }),
-      // isDevMode && new BundleAnalyzerPlugin(),
     ].filter(Boolean),
     output: {
       path: path.resolve(__dirname + "/dist"),
@@ -63,10 +44,6 @@ module.exports = function (env) {
       publicPath: "/",
       clean: true,
     },
-    // optimization: {
-    //   minimize: true,
-    //   runtimeChunk: "single",
-    // },
     ...(isDevMode && {
       devServer: {
         static: { directory: path.join(__dirname, "dist") },
